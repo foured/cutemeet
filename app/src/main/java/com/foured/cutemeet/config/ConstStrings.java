99ae801b6b7d82dba0bf4bcf9d06c8fe61dad755
Ivan Toporov <toporov07@inbox.ru>
2025-02-16 16:28:55 +0300

    idfk

--- app/src/main/java/com/foured/cutemeet/config/ConstStrings.java
+++ app/src/main/java/com/foured/cutemeet/config/ConstStrings.java
@@ -1,7 +1,7 @@
 package com.foured.cutemeet.config;
 
 public class ConstStrings {
-    public static final String serverAddress = "http://84.252.142.125:8080";
+    public static final String serverAddress = "http://84.252.141.8:8080";
 
     public static final String sharedPreferencesUserDataPath = "UserAccountLocalData";
     public static final String sharedPreferences_usernameKey = "username";
